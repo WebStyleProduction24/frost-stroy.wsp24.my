bc73b3424284e4c865c2a7d0b3c19733034834d4
Jack36vrn <Jack36@list.ru>
2018-05-14 19:18:37 +0300

    v.3.1
    Устранен баг с видео для десктопа, необходима проверка для мобильных устройств

--- www/wp-content/themes/frost-stroy/header.php
+++ www/wp-content/themes/frost-stroy/header.php
@@ -6,8 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<title><?php echo wp_get_document_title(); ?></title>
-		<?php wp_head(); ?>
+	<?php wp_head(); ?>
 </head>
 
 <body>
-    
