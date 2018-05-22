69b4fa9f75b9442b49c8d10d8adcda278e2596b2
Jack36vrn <Jack36@list.ru>
2018-05-22 23:00:36 +0300

    v.3.2.1
    1. blog.php переименован на front-page.php
    2. В index.php начата работа на отображением страницы блога, изменена шапка блога
    3. Создан css/blog.css для хранения css непосредственного блога

---
+++ www/wp-content/themes/frost-stroy/front-page.php
@@ -0,0 +1,42 @@
+<?php get_header(); ?>
+
+<header class="header-land">
+    <div class="content">
+        <div class="left">
+            <?php the_custom_logo(); ?>
+            <a href="<?php echo home_url();?>" class="logo"><?php echo get_bloginfo('name');?></a>
+            <?php wp_nav_menu(array(
+                'theme_location' => 'land',
+                'container'       => 'div',
+                'container_class' => 'nav'
+            ));?>
+        </div>
+        <div class="tel" style="float: right;">
+            <p><?php echo get_option( 'eg_setting_name2' );?></p>
+            <a href="tel:<?php echo tel();?>"><?php echo get_option( 'eg_setting_name' );?></a>
+        </div>
+    </div>
+</header>
+
+<main>
+    <div class="content">
+        <?php if (have_posts()): while (have_posts()): the_post(); ?>
+            <?php the_content(); ?>
+        <?php endwhile; endif; ?>
+    </div>
+</main>
+<script>
+
+    $('.video_id').on('canplay', function () {
+        $(this).attr('poster', '');
+    });
+    $("body").on("mouseover", "video", function(){
+        this.play();
+        $( this ).css( "background-color", "black" );
+    });
+    $("body").on("mouseleave", "video", function(){
+        this.pause();
+    })
+</script>
+
+<?php get_footer(); ?>
